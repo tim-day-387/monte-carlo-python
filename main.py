68d741ec5a549f459de55dcdc9bd5bd1347b0b4e
Tim Day <tday141@gmail.com>
2021-07-08 21:57:46 -0400

    Changed main to produce reports

--- main.py
+++ main.py
@@ -14,100 +14,60 @@ from grabAndDuckPlayer import grabAndDuckPlayer
 from mctsPlayer import mctsPlayer
 from deck import deck
 from game import game
-
-# How Arguments Work
-# python3 Monster.py quiteMode AIplayerType seed
-# quiteMode - 0 or 1
-# AIplayerType - 0, 1, 2, 3, 4 for random, grab and duck, rollouts, mcts, perform comparison
-# vsType 0 or 1, 0 for opponents random, 1 for grab and duck
-# seed - any text, optional
-
-# Right Number of Arguments
-numArgs = len(sys.argv[1:])
-if numArgs < 2:
-    print("Wrong number of arguments.") 
-    exit()
-
-# AI and Enemy selection
-AIselection = sys.argv[2]
-vsType = sys.argv[3]
-
-# Quite mode?
-if sys.argv[1] == '1':
-    qMode = True
-else:
-    qMode = False
     
-# Set Seeds
-if numArgs >= 4:
-    random.seed(sys.argv[4])
-else:
-    random.seed()
-    
-# Select kind of player up against, by defining "EnemyPlayer" to mean one of two classes
-if vsType == '0': 
-    enemyPlayer=randomPlayer
-elif vsType == '1':
-    enemyPlayer=grabAndDuckPlayer
-else:
-    print("Bad player type! Must be 0 or 1")
-    exit()
-    # Code for Playing Games
+# Return results of a set of games
+def playGames(numGames, playerAI, enemyAI):
+    Results=0
 
-# Determine what kind of results to return
-if AIselection == '4':
-    Games=20
-    
-#    MctsResults=0
-#    for i in range(Games):
-#        players = []
-#        players.append(enemyPlayer("Foo"))
-#        players.append(mctsPlayer("AI"))
-#        players.append(enemyPlayer("Bar"))
-#        theGame = game(players)
-#
-#        if theGame.play()[0] == "AI":
-#            MctsResults+=1
-
-    GDResults=0
-    for i in range(Games):
-        players = []
-        players.append(enemyPlayer("Foo"))
-        players.append(grabAndDuckPlayer("AI"))
-        players.append(enemyPlayer("Bar"))
-        theGame = game(players)
-
-        if theGame.play()[0] == "AI":
-            GDResults+=1
-
-    RResults=0
-    for i in range(Games):
-        players = []
-        players.append(enemyPlayer("Foo"))
-        players.append(rolloutPlayer("AI"))
-        players.append(enemyPlayer("Bar"))
-        theGame = game(players)
-
-        if theGame.play()[0] == "AI":
-            RResults+=1
-            
-#    print("MTCS AI win rate: ", 100*MctsResults/Games,"% or",MctsResults,"over",Games)
-    print("Grab And Duck AI win rate: ", 100*GDResults/Games,"% or",GDResults,"over",Games)
-    print("Rollout AI win rate: ", 100*RResults/Games,"% or",RResults,"over",Games)
-else:
-    players = []
-    players.append(enemyPlayer("Foo"))
-
-    if AIselection == '0':
-        players.append(randomPlayer("AI"))
-    elif AIselection == '1':
-        players.append(grabAndDuckPlayer("AI"))
-    elif AIselection == '2':
-        players.append(rolloutPlayer("AI"))
+    # Decide enemyAI
+    if enemyAI == 0:
+        enemyPlayer=randomPlayer
     else:
-        players.append(mctsPlayer("AI"))
-        
-    players.append(enemyPlayer("Bar")) 
-    theGame = game(players) 
+        enemyPlayer=grabAndDuckPlayer
 
-    print(theGame.play())
+    # Create games and play them
+    for i in range(numGames):
+        players = []
+        players.append(enemyPlayer("Foo"))
+
+        # Decide playerAI type
+        if playerAI == 0:
+            players.append(grabAndDuckPlayer("AI"))
+        elif playerAI == 1:
+            players.append(rolloutPlayer("AI"))
+        else:
+            players.append(mctsPlayer("AI"))
+            
+        players.append(enemyPlayer("Bar"))
+        theGame = game(players)
+
+        # Record results if playerAI wins
+        if theGame.play()[0] == "AI":
+            Results+=1
+
+    # Decide playerAI type
+    if playerAI == 0:
+        print("** Grab And Duck **")
+    elif playerAI == 1:
+        print("**** Rollouts *****")
+    else:
+        print("****** MCTS *******")
+
+    # Return results
+    print("AI win rate: ", 100*(Results/numGames),"% or",Results,"over",numGames)
+
+# Produce all reports for numGames
+def playAll(numGames):
+    for enemyAI in range(0,2):
+        for playerAI in range(0,1):
+            playGames(numGames, playerAI, enemyAI)
+
+# Set Seeds
+numArgs = len(sys.argv[1:])
+if numArgs != 0:
+    random.seed(sys.argv[1])
+else:
+    random.seed("bababooey")
+            
+# Produce report
+playAll(20)
