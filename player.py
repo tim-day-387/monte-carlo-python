dafa6bfa6f552d5b2f3b9a81bf3d94ab4108f219
Tim Day <tday141@gmail.com>
2021-07-11 00:28:08 -0400

    Cleaned up player

--- player.py
+++ player.py
@@ -2,19 +2,19 @@
 import abc
 
 # Class for player
-class player(metaclass=abc.ABCMeta):          # This is an abstract base class.
+class player(metaclass=abc.ABCMeta):
     def __init__(self, name):
         self.name = name
-        self.hand = []         # List of cards (tuples). I don't think this needs to be a class....
+        self.hand = []
         self.score = 0
         self.zombie_count = 0
-        
-    def __repr__(self):        # If __str__ is not defined this will be used. Allows easy printing
-        # of a list of these, e.g. "print(players)" below.
+
+    # If __str__ is not defined this will be used. Allows easy printing of list of players    
+    def __repr__(self):        
         return str(self.name) + ": " + str(self.hand) + "\n"
-    
+
+    # A player shows legal cards instead of playing one, only works if yieldMode is true
     @abc.abstractmethod
     def playCard(self):
         pass
-    # a player who shows legal cards instead of playing one.
-    # this only works if yieldMode is true
+    
