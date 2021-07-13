6debfcfee75ebff213de60299c1d60ca7de5a8e8
Tim Day <tday141@gmail.com>
2021-07-12 21:39:23 -0400

    Cleaned the timeAllocator class

--- timeAllocator.py
+++ timeAllocator.py
@@ -1,23 +1,37 @@
 # Class for timeAllocator
 class timeAllocator:
-    def __init__(self,initialTime,priorityMul=1.0):
-        self.iniTime=initialTime
-        self.curTime=initialTime
-        self.prio=priorityMul
+    # Constructor
+    def __init__(self, initialTime, priorityMul = 1.0):
+        self.iniTime = initialTime
+        self.curTime = initialTime
+        self.prio = priorityMul
+
+    # Reset the given time
     def reset(self):
-        self.curTime=self.iniTime
-    def getAllowedTime(self,handSize,legalSize): #How much time should I be allowed? Legal Size is how many legal cards there are.
-        if handSize==2: #if there's only 2 cards in hand, just give all the hand.
+        self.curTime = self.iniTime
+
+    # How much time should I be allowed?
+    def getAllowedTime(self, handSize, legalSize):
+        # If there's only 2 cards in hand, just give all the hand.
+        if handSize == 2: 
             return self.curTime
-        if self.curTime <= 0.0 : #if we have no time left
+
+        # If we have no time left, return nothing
+        if self.curTime <= 0.0 : 
             return 0
-        #otherwise, we need to compute the ratio
-        ratio=(handSize*legalSize)/(handSize*(legalSize+(handSize-1)*(2*handSize-1)/6)-1) #how much time should this get, vs how many are left, in the worst case? 
-        #explanation in the writeup
-        if ratio*self.prio >= 0.99: #if we'd give almost all the time
-            pass #ignore the priority
+        
+        # Otherwise, compute the ratio (how much time this get v. how many left in worst case?) 
+        ratio = (handSize*legalSize)/(handSize*(legalSize+(handSize-1)*(2*handSize-1)/6)-1) 
+
+        # If we'd give almost all the time, ignore priority
+        if ratio*self.prio >= 0.99: 
+            pass 
         else:
-            ratio*=self.prio #otherwise, apply it.
+            ratio*=self.prio
+
+        # Return the remaining time
         return self.curTime*ratio
+
+    # Remove the spent time
     def removeSpent(self,spentTime):
         self.curTime-=spentTime
