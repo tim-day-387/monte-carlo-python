1783b50847f877c28dff0cd902babb85cb6885b1
Tim Day <tday141@gmail.com>
2021-07-13 22:51:36 -0400

    Cleaned up the random player class

--- randomPlayer.py
+++ randomPlayer.py
@@ -2,24 +2,24 @@
 from player import player
 
 # Class for randomPlayer
-class randomPlayer(player):  # Inherit from Player
+class randomPlayer(player):
+    # Constructor
     def __init__(self, name):
         super().__init__(name)
 
+    # Decide which card to play
     def playCard(self, trick,game):
-        # added the game itself, since the AI needs that, even if it isn't used here.
-        # print("-", self.name+"("+str(self.score)+")(Z"+str(self.zombie_count)+")", "sees", trick)
-        # comment out for quiet mode
+        # Check if trick is empty
         if len(trick) != 0:
             # Figure out what was led and follow it if we can
             suit = trick[0][0]
-            # print(self.name, ":", suit, "was led")
+            
             # Get the first occurence of a matching suit in our hand
-            # This 'next' thing below is a "generator expression"
             card_idx = next((i for i,c in enumerate(self.hand) if c[0]==suit), None)
+
+            # Play the card, if it exists
             if card_idx != None:
                 return self.hand.pop(card_idx)
-            # If the trick is empty or if we can't follow suit, return anything
+            
+        # If the trick is empty or if we can't follow suit, return anything
         return self.hand.pop()
-    # block of helper keys for grab and duck
-    # each one places prefered cards at the end of the list,for pop.
