1eccef5eef0f5be6b5d93af123ad590076becff4
Tim Day <tday141@gmail.com>
2021-07-14 21:42:23 -0400

    Cleaned up the rollout player class

--- rolloutPlayer.py
+++ rolloutPlayer.py
@@ -10,6 +10,7 @@ from timeAllocator import timeAllocator
 
 # Class for rolloutPlayer
 class rolloutPlayer(player):
+    # Constructor
     def __init__(self, name, TIME_GIVEN, timeAlloc = False):
         # Inherit
         super().__init__(name)
@@ -24,78 +25,130 @@ class rolloutPlayer(player):
         # Set remaining parameters
         self.TIME_GIVEN = TIME_GIVEN
 
-    def playCard(self, trick,game): # game is only used to make a virtual copy, does not hand look
-        if len(self.hand)==0: #check if I'm being asked to play when I can't.
+    # Decide what card to play
+    def playCard(self, trick, game):
+        # Check if there are cards
+        if len(self.hand) == 0: 
             raise Exception("My hand is empty, why are you asking for a card?")
-        if self.allocator==False: #if we're not using the question 6 allocator,
-            terminateBy=time.process_time()+self.TIME_GIVEN # set a timer for one TIME GIVEN
+
+        # Check if the allocator is being used
+        if self.allocator == False:
+            # Set a timer for one TIME GIVEN
+            terminateBy=time.process_time()+self.TIME_GIVEN 
         else:
-            if len(self.hand)==18: #if our hand is full
-                self.allocator.reset() #reset our time allocator.
+            # If hand is full reset our time allocator
+            if len(self.hand)==18:
+                self.allocator.reset()
+
+            # Set start and end times
             startAt=time.process_time()
-            terminateBy=startAt+18.0 #this is a temporary value, is corrected once we know how many cards are legal.
-            
-        legalCards=[] #on the first rollout, find what cards in the hand are legal, which are in lists of "number of times, total advantage"
-        #if there is only one legal move, just play it and avoid computing. Advantage is me-first place, or if I am in first, me-second place. 
-        #Set to +200 if I win on the hand, set to -200 if I lose on the hand, as winning or losing on the spot is way more important than next round.
-        tryThis=0 #which index in the list should we start with this time?
-        while time.process_time() < terminateBy: # loop until time has finished.
-            temp = game.makeVirtualGameCopy(trick) #Note, this also returns the imagined hands of Alice and Bob, but we can get rid of those
-            gameGen = temp[0] #Get the game generator object.
-            del temp #get rid of temporary object. may remove if it causes problems
-            if len(legalCards)==0: #if we don't know what cards are legal, find out.
-                temp=next(gameGen) #get the first return from the game generator, which gives a lot more than just our hand
-                if type(temp[2])==type(1): #if the next thing will throw an exception...
-                    raise TypeError("Something is wrong with my first return!"+str(temp)) #this doesn't seem to happen, but it might
-                if len(temp[2])==1: #if there's only one legal card, no point in doing anything else.
-                    self.hand.remove(temp[2][0]) #make sure to actually remove the card.
-                    return temp[2][0] #temp[2] is the legal card list, so get the only element and return
-                #otherwise, format the legal card list.
-                for card in temp[2]: #for each card...
-                    legalCards.append([card,0,0]) #add the card, and that it has been tried zero times, and has had advantage of zero
+            terminateBy=startAt+18.0
+
+        # Find what cards in the hand are legal    
+        legalCards=[]
+
+        # Which should we start with?
+        tryThis=0
+
+        # Loop until time has finished
+        while time.process_time() < terminateBy:
+            # Make copy of game
+            temp = game.makeVirtualGameCopy(trick)
+
+            # Get the game generator object.
+            gameGen = temp[0]
+
+            # Get rid of temp object
+            del temp
+
+            # If we don't know what cards are legal, find out
+            if len(legalCards) == 0:
+                # Get the first return from the game generator
+                temp = next(gameGen)
+
+                # Check if there is an error with the return
+                if type(temp[2])==type(1): 
+                    raise TypeError("Something is wrong with my first return!"+str(temp)) 
+
+                # If one card, remove and return it
+                if len(temp[2])==1: 
+                    self.hand.remove(temp[2][0]) 
+                    return temp[2][0]
+                
+                # Otherwise, format the legal card list.
+                for card in temp[2]:
+                    # Add card, record it has been tried zero times, has had advantage zero
+                    legalCards.append([card,0,0]) 
+
+                # Get rid of temp object
                 del temp
                 
-                #then, see if we need to update how much time we're allowed.
-                if self.allocator!=False: #that is, we have an allocator
-                    terminateBy=startAt+self.allocator.getAllowedTime(len(self.hand),len(legalCards)) #set up the real terminate time
-                    if time.process_time() > terminateBy: #if we actually ran out of time, just return pthe first card.
-                        self.hand.remove(legalCards[0][0]) #make sure we get the played card out of the hand
+                # Update how much time we're allowed.
+                if self.allocator != False: 
+                    # Set up the real terminate time
+                    terminateBy = startAt+self.allocator.getAllowedTime(len(self.hand),
+                                                                        len(legalCards))
+
+                    # If we've run out of time, return first card
+                    if time.process_time() > terminateBy: 
+                        self.hand.remove(legalCards[0][0]) 
                         return legalCards[0][0]
                     
-            else: #if we already know what cards are legal, just run the "next" without looking at the output.
-                next(gameGen) #get it ready to accept the card.
-            #Now that I know Legal Cards is full, we can try the card at index tryThis.
-            output=gameGen.send(legalCards[tryThis][0]) #send card tryThis and collect the output.
-            legalCards[tryThis][1]+=1 #increment how many times this was tried
-            #then, play the hand out.
-            while time.process_time() < terminateBy: #will break out if the hand is over before then.
-                if output[0]==True: #if we are told the game is over...
-                    if output[1]==True: #if someone won the game...
-                        if output[2]==1: #is it us?
-                            legalCards[tryThis][2]+=200 #if it is us, add 200 points to advantage!
+            else:
+                # Get generator ready to accept the card.
+                next(gameGen)
+
+            # Legal Cards is full, try the card at tryThis, increment how many times it was tried
+            output=gameGen.send(legalCards[tryThis][0])
+            legalCards[tryThis][1]+=1
+            
+            # Play the hand out
+            while time.process_time() < terminateBy:
+                # Is the game over?
+                if output[0] == True:
+                    # Has someone won the game?
+                    if output[1] == True:
+                        # Did the player win?
+                        if output[2]==1:
+                            # Add to advantage
+                            legalCards[tryThis][2]+=200 
                         else:
-                            legalCards[tryThis][2]-=200 #If someone else won, we lost, so remove 200 points
-                        #in either case, the game is over
+                            # Remove from advantage
+                            legalCards[tryThis][2]-=200 
+
+                        # End of game
                         break
-                    #otherwise, we need to look at points scored.
-                    adv=output[4] - max(output[3],output[5]) #take our score, and subtract the larger of the other players.
-                    #then add it to the total advantage for this card.
+
+                    # Otherwise, take our score, and subtract the larger of the other players.
+                    adv = output[4] - max(output[3],output[5]) 
+
+                    # Add it to the total advantage for this card.
                     legalCards[tryThis][2]+=adv
-                    #finally, break.
+
+                    # End of game
                     break
-                #otherwise, we're being asked to play a card.
-                card=output[2][0] #This one picks the first legal card in the hand of whoever's turn it is. Mcts will have to care whose turn it is, but I don't.
-                #also if you simulated everyone with grab and duck, they would need to know the trick from this output. but we don't have to.
-                #finally, send that card in and collect the new output.
+
+                # Otherwise, play the first legal card in the hand of whoever's turn it is
+                card=output[2][0] 
+                
+                # Send that card in and collect the new output.
                 output = gameGen.send(card)
-            #now, the hand has come to an end.
-            tryThis=(tryThis+1)%len(legalCards) #move to trying the next card in sequence, or back to the start.
-        #okay, so we've collected as much data as we can in one second. Now comes decision time.
-        legalCards.sort(key=lambda entry: entry[2]/(entry[1]+1)) #assume each one has been tried at least once, or this will set on fire.
+
+            # The hand has come to an end
+            # Move to trying the next card in sequence, or back to the start.
+            tryThis = (tryThis+1)%len(legalCards) 
+
+        # We've collected as much data as we can in one second
+        # Sort cards by advantage
+        legalCards.sort(key=lambda entry: entry[2]/(entry[1]+1)) 
         
-        #if we are using an allocator, we need to update it with how much time was spent
+        # If using an allocator, update it with how much time was spent
         if self.allocator != False:
             self.allocator.removeSpent(time.process_time()-startAt)
-        self.hand.remove(legalCards[-1][0]) #remove the card from hand
-        return legalCards[-1][0] #return the card at the end of the legal cards list, and strip off how much it was tried.
+
+        # Remove the card from hand
+        self.hand.remove(legalCards[-1][0])
+
+        # Return card at the end of the legal cards list, strip off how much it was tried
+        return legalCards[-1][0] 
 
